40da52d697915dbb8c91ccae79fbf077af97a628
Stefan Schmidt <thrimbor.github@gmail.com>
2015-10-27 20:27:03 +0100

    create Texture, error checking, cleanup on exit

--- src/gfxlib2/unix/gfx_driver_sdl2.c
+++ src/gfxlib2/unix/gfx_driver_sdl2.c
@@ -7,33 +7,75 @@ typedef struct SDL2_GFXDRIVER_CTX_
 {
 	SDL_Window *screen;
 	SDL_Renderer *ren;
+	SDL_Texture *canvas;
+	int initialized;
 } SDL2_GFXDRIVER_CTX;
 
-static SDL2_GFXDRIVER_CTX __sdl2_ctx;
+static SDL2_GFXDRIVER_CTX __sdl2_ctx = {NULL, NULL, NULL, FALSE};
 
 static int driver_init(char *title, int w, int h, int depth, int refresh_rate, int flags)
 {
 	if (w==0 || h==0 || depth==0)
 		return 0;
 	
-	// FIXME: only do this the first time
-	// FIXME: check for error in return value
-	SDL_Init(SDL_INIT_VIDEO);
+	/* initialize SDL on first call */
+	if (!__sdl2_ctx.initialized)
+	{
+		if (SDL_Init(SDL_INIT_VIDEO) != 0)
+		{
+			return -1;
+		}
+	}
 	
 	// FIXME: We should clean up old state here
 	
 	__sdl2_ctx.screen = SDL_CreateWindow(title, 100, 100, w, h, SDL_WINDOW_SHOWN);
-	// FIXME: check for errors here
+	if (__sdl2_ctx.screen == NULL)
+	{
+		SDL_Quit();
+		return -1;
+	}
 	
 	__sdl2_ctx.ren = SDL_CreateRenderer(__sdl2_ctx.screen, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-	// FIXME: check for errors here
+	if (__sdl2_ctx.ren == NULL)
+	{
+		SDL_DestroyWindow(__sdl2_ctx.screen);
+		SDL_Quit();
+		return -1;
+	}
 	
+	__sdl2_ctx.canvas = SDL_CreateTexture(__sdl2_ctx.ren, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, w, h);
+	if (__sdl2_ctx.canvas == NULL)
+	{
+		SDL_DestroyRenderer(__sdl2_ctx.ren);
+		SDL_DestroyWindow(__sdl2_ctx.screen);
+		SDL_Quit();
+		return -1;
+	}
+	
+	SDL_RenderClear(__sdl2_ctx.ren);
+	SDL_RenderPresent(__sdl2_ctx.ren);
+	
+	__sdl2_ctx.initialized = TRUE;
 	return 0;
 };
 
 static void driver_exit(void)
 {
+	if (__sdl2_ctx.initialized)
+	{
+		if (__sdl2_ctx.screen != NULL)
+		{
+			SDL_DestroyRenderer(__sdl2_ctx.ren);
+			SDL_DestroyWindow(__sdl2_ctx.screen);
+			SDL_DestroyTexture(__sdl2_ctx.canvas);
+			__sdl2_ctx.screen = NULL;
+		}
+		
+		SDL_Quit();
+	}
 	
+	return 0;
 }
 
 static void driver_lock(void)
